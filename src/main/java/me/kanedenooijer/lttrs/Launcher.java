c1f696e76a1cec0782f6061b731de81ebc78ab7b
Kane de Nooijer <kanedenooijer@gmail.com>
2026-02-23 22:34:05 +0100

    Added runnable JAR support

---
+++ src/main/java/me/kanedenooijer/lttrs/Launcher.java
@@ -0,0 +1,14 @@
+package me.kanedenooijer.lttrs;
+
+import javafx.application.Application;
+
+/**
+ * Entry point to launch the JavaFX application.
+ */
+public final class Launcher {
+
+    static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+
+}
